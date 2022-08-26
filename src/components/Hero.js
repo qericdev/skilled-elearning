329a1af168e574b676f41260636716ede4b60a64
Erick Rojas <qericdev@gmail.com>
2022-08-26 17:49:46 -0500

    fixed section tag

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,16 +3,16 @@ import styles from './../styles/Hero.module.css';
 function Hero() {
     return (
         <div className={styles.container}>
-            <section className={styles.description}>
+            <div className={styles.description}>
                 <h1>Maximize skill, minimize budget</h1>
                 <p>Our modern courses across a range of in-demand skills will give you the knowledge you need to live the life you want.</p>
                 <button>Get Started</button>
-            </section>
-            <section className={styles.image}>
+            </div>
+            <div className={styles.image}>
                 <img className={styles.imgActiveDesktop} alt="course-hours-and-member" src="./../../images/image-hero-desktop@2x.webp"/>
                 <img className={styles.imgActiveTablet} alt="course-hours-and-member" src="./../../images/image-hero-tablet@2x.webp"/>
                 <img className={styles.imgActiveMobile} alt="course-hours-and-member" src="./../../images/image-hero-mobile@2x.webp"/>
-            </section>
+            </div>
         </div>
     );
 }
