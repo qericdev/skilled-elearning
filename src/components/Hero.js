daf0afb2443fe8e19b13d0d4e617ca2a973cab41
Erick Rojas <qericdev@gmail.com>
2022-08-26 08:11:44 -0500

    Navbar Responsive for phones was added

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import styles from './../styles/Hero.module.css';
+
+function Hero() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Hero;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './../styles/Navbar.module.css';
 
 
